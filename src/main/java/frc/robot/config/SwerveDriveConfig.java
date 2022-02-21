ed4ddcf2319f4eb6f1038debd1f3c0b6e290a837
Michael Kaatz <michaelkaatz2012@hotmail.com>
2022-02-21 13:22:05 -0500

    Add swerve config structs

---
+++ src/main/java/frc/robot/config/SwerveDriveConfig.java
@@ -0,0 +1,7 @@
+package frc.robot.config;
+
+public class SwerveDriveConfig {
+
+	public SwerveModuleConfig moduleConfig;
+	public double gyroFactor;
+}
---
+++ src/main/java/frc/robot/config/SwerveModuleConfig.java
@@ -0,0 +1,18 @@
+package frc.robot.config;
+
+import com.ctre.phoenix.motorcontrol.can.SlotConfiguration;
+
+public class SwerveModuleConfig {
+
+	public SlotConfiguration pid;
+	public double maxRamp;
+	public boolean steerBrake;
+
+	public boolean steerCurrentLimitEnabled;
+	public double steerCurrentLimit;
+	public double steerCurrentLimitTime;
+
+	public boolean driveCurrentLimitEnabled;
+	public double driveCurrentLimit;
+	public double driveCurrentLimitTime;
+}
