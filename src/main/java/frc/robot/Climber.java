e12e630bacbedd545fc6190492ee13891f159e13
Michael Kaatz <michaelkaatz2012@hotmail.com>
2022-03-01 10:01:28 -0500

    Reduce climber status frame frequencies

--- src/main/java/frc/robot/Climber.java
+++ src/main/java/frc/robot/Climber.java
@@ -3,6 +3,7 @@ package frc.robot;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
 import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
@@ -46,6 +47,16 @@ public class Climber implements Subsystem {
 		leftMotor.configAllSettings(leftConfig, 1000);
 		leftMotor.setInverted(true);
 		leftMotor.setNeutralMode(NeutralMode.Brake);
+		leftMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_1_General, 10);
+		leftMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_2_Feedback0, 20);
+		leftMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_3_Quadrature, 255);
+		leftMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_4_AinTempVbat, 255);
+		leftMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_8_PulseWidth, 255);
+		leftMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_10_Targets, 255);
+		leftMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_12_Feedback1, 255);
+		leftMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0, 255);
+		leftMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_14_Turn_PIDF1, 255);
+		leftMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_Brushless_Current, 255);
 
 		rightMotor = new TalonFX(rightChannel);
 		TalonFXConfiguration rightConfig = new TalonFXConfiguration();
@@ -58,6 +69,16 @@ public class Climber implements Subsystem {
 		rightMotor.setInverted(TalonFXInvertType.OpposeMaster);
 		rightMotor.setNeutralMode(NeutralMode.Brake);
 		rightMotor.follow(leftMotor);
+		rightMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_1_General, 255);
+		rightMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_2_Feedback0, 255);
+		rightMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_3_Quadrature, 255);
+		rightMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_4_AinTempVbat, 255);
+		rightMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_8_PulseWidth, 255);
+		rightMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_10_Targets, 255);
+		rightMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_12_Feedback1, 255);
+		rightMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0, 255);
+		rightMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_14_Turn_PIDF1, 255);
+		rightMotor.setStatusFramePeriod(StatusFrameEnhanced.Status_Brushless_Current, 255);
 
 		limitSwitch = new DigitalInput(limitSwitchPort);
 
